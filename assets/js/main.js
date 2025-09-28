17dcce2094abb77f7ded810adf987255f4f4df6c
PJ15Hash <PJ15Hash@users.noreply.github.com>
2025-09-28 20:21:50 +0000

    deploy: 3102f8c72b36ead7907acb4ee72139e3648383a5

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,13 @@ document.addEventListener('DOMContentLoaded', function(){
     });
   });
 });
+
+// Mobile Menu Toggle
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("menu-toggle");
+  const navLinks = document.getElementById("nav-links");
+
+  toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
